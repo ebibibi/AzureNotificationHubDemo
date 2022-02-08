062e276bba1092edd85587101b82b5a65a8684cf
Masahiko Ebisuda <ebibibi@gmail.com>
2022-02-08 19:41:14 +0900

    動くバージョン

--- app/src/main/java/com/example/notificationhubdemo/CustomNotificationListener.java
+++ app/src/main/java/com/example/notificationhubdemo/CustomNotificationListener.java
@@ -1,12 +1,27 @@
 package com.example.notificationhubdemo;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.util.Log;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.RemoteMessage;
 import com.microsoft.windowsazure.messaging.notificationhubs.NotificationListener;
 
 import java.util.Map;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+
+
 
 public class CustomNotificationListener implements NotificationListener {
 
@@ -15,17 +30,28 @@ public class CustomNotificationListener implements NotificationListener {
         RemoteMessage.Notification notification = remoteMessage.getNotification();
         String title = notification.getTitle();
         String body = notification.getBody();
+        String tag = "Azure Notification Hub";
         Map<String, String> data = remoteMessage.getData();
+        Log.d("test3", "test3");
+        Log.d(tag, "Message Notification Title: " + title);
+        Log.d(tag, "Message Notification Body: " + remoteMessage);
 
-        if(remoteMessage != null) {
-            Log.d("xxxxxxxxxxxx", "Message Notification Title: " + title);
-            Log.d("xxxxxxxxxxxx", "Message Notification Body: " + remoteMessage);
-        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "Azure")
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-        if(data != null) {
-            for(Map.Entry<String, String> entry : data.entrySet()) {
-                Log.d("xxxxxxxxxxxx", "key, " + entry.getKey() + " value " + entry.getValue());
-            }
-        }
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(1, builder.build());
+
+
+
+
     }
+
+
+
 }
--- app/src/main/java/com/example/notificationhubdemo/MainActivity.java
+++ app/src/main/java/com/example/notificationhubdemo/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.notificationhubdemo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -16,13 +21,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         NotificationHub.setListener(new CustomNotificationListener());
 
-        Log.d("xxxxxxxxxxxx", "Test");
+        Log.d("Azure Notification Hub", "test6");
 
+        createNotificationChannel();
 
         String hubName = "jbs-notificationHub";
-        String connectionString = "Endpoint=sb://jbs-notificationHub-namespace.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=PBH1OJ6tCr3WxrNkX1PkknIhJPsaHi0lFxGs2/N6I3U=";
+        String connectionString = "Endpoint=sb://jbs-notificationHub-namespace.servicebus.windows.net/;SharedAccessKeyName=test;SharedAccessKey=b1MbWxukspIS0ic8Ch9XumdlN5rwzk9ig9YWZQ8MTOg=";
 
         NotificationHub.start(this.getApplication(), hubName, connectionString);
 
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Azure Notification Hub";
+            String description = "Azure Notification Hub Sample";
+            NotificationChannel channel = new NotificationChannel("Azure", name, NotificationManager.IMPORTANCE_MAX);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
